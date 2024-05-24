fe56f297850ecb9b0cd4a35475f60ddc198c30b6
Paul <paulcoignac@gmail.com>
2024-05-24 14:14:17 +0200

    Export pdf

--- frontend/src/app/pages/results/results.component.ts
+++ frontend/src/app/pages/results/results.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {CompetitionsService} from "../../services/competitions.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Paths} from "../../statics/Paths";
 import {CompetitionModel} from "../../models/competition.model";
 import {TargetModel} from "../../models/target.model";
-import {EventEnum} from "../../models/event.enum";
-import {StageEnum} from "../../models/stage.enum";
 import {AsyncPipe, KeyValuePipe, NgForOf, NgIf} from "@angular/common";
 import {ButtonModule} from "primeng/button";
 import {NamePipe} from "../../pipes/name.pipe";
 import {SharedModule} from "primeng/api";
 import {TableModule} from "primeng/table";
+import {PanelModule} from "primeng/panel";
+import html2canvas from 'html2canvas';
+import jsPDF from "jspdf";
 
 class Column {
   field: string;
@@ -29,12 +30,16 @@ class Column {
     ButtonModule,
     NamePipe,
     SharedModule,
-    TableModule
+    TableModule,
+    PanelModule
   ],
   templateUrl: './results.component.html',
   styleUrl: './results.component.scss'
 })
 export class ResultsComponent {
+
+  @ViewChild('pdfrender') pdfrender: ElementRef;
+
   get targets(): TargetModel[] {
     return this._targets;
   }
@@ -47,8 +52,8 @@ export class ResultsComponent {
         if (this.results.get(this.getLibelleEpreuve(target))?.has(this.getLibelleCategorie(target))) {
           this.results.get(this.getLibelleEpreuve(target))?.set(this.getLibelleCategorie(target),
 
-          [...(this.results.get(this.getLibelleEpreuve(target))?.get(this.getLibelleCategorie(target))||[]), target].sort((a, b) => b.totalScore - a.totalScore)
-            );
+            [...(this.results.get(this.getLibelleEpreuve(target))?.get(this.getLibelleCategorie(target)) || []), target].sort((a, b) => b.totalScore - a.totalScore)
+          );
         } else {
           this.results.get(this.getLibelleEpreuve(target))?.set(this.getLibelleCategorie(target), [target]);
         }
@@ -58,21 +63,22 @@ export class ResultsComponent {
       }
     });
   }
+
   cols: Column[] = [
-    { field: 'competitorId', header: 'Compétiteur' },
-    { field: 'totalScore', header: 'Score' },
+    {field: 'competitorId', header: 'Compétiteur'},
+    {field: 'totalScore', header: 'Score'},
   ];
+
   getLibelleEpreuve(target: TargetModel) {
 
-    return  [target.event,target.stage].filter((e) => !!e).join(' - ');
+    return [target.event, target.stage].filter((e) => !!e).join(' - ');
   }
 
   getLibelleCategorie(target: TargetModel) {
-    return [target.competitor.category,target.competitor.sex?.charAt(0)].filter((e) => e !== undefined).join(' - ');
+    return [target.competitor.category, target.competitor.sex?.charAt(0)].filter((e) => e !== undefined).join(' - ');
   }
 
-  results: Map<string, Map<string,TargetModel[]>>
-
+  results: Map<string, Map<string, TargetModel[]>>
 
 
   private competition: CompetitionModel;
@@ -82,6 +88,7 @@ export class ResultsComponent {
     private competitionsService: CompetitionsService,
     private route: ActivatedRoute,
     private router: Router,
+    private elementRef: ElementRef
   ) {
     const id = this.route.snapshot.paramMap.get('id');
     if (!id) {
@@ -97,6 +104,44 @@ export class ResultsComponent {
   loadTargets() {
     this.competitionsService.getTargets(this.competition.id).then((data) => {
       this.targets = [...data];
+    });
+  }
+
+  saveAsPDF() {
+    const data = this.pdfrender.nativeElement;
+    html2canvas(data).then(canvas => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF({
+        orientation: 'p',
+      });
+      pdf.html(data, {
+        html2canvas: {
+          scale: 0.21,
+        },
+
+        callback: function (pdf) {
+          pdf.save('resultats.pdf');
+        },
+        margin: 10,
+
+      });
+
+      // //add pages if content is too long, image must be split in multiple pages
+      // const imgWidth = 297;
+      // const pageHeight = 210;
+      // const imgHeight = canvas.height * imgWidth / canvas.width;
+      // let heightLeft = imgHeight;
+      // let position = 0;
+      // pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      // heightLeft -= pageHeight;
+      // while (heightLeft >= 0) {
+      //   position = heightLeft - imgHeight;
+      //   pdf.addPage();
+      //   pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      //   heightLeft -= pageHeight;
+      // }
+      // pdf.save('resultats.pdf');
+
     });
   }
 }
